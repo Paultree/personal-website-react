3166a8f6ea8abc1444846932b65c9a900ce3c617
paul pham <paulpham3000@gmail.com>
2022-12-28 20:26:50 +1100

    basic files

---
+++ personal-website-react/src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from "./assets/react.svg";
+import styles from "./App.module.scss";
+import Header from "./containers/Header/Header";
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ personal-website-react/src/containers/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Hi, I'm Paul.</h1>
+      <h2>Junior Full-Stack Developer</h2>
+      <h1>-</h1>
+    </div>
+  );
+};
+
+export default Header;
