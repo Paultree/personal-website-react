f2c5988d1ffcbbb913864e49996be854686e0158
paul pham <paulpham3000@gmail.com>
2023-01-12 21:29:15 +1100

    img update

--- personal-website-react/src/components/AboutCard/AboutCard.jsx
+++ personal-website-react/src/components/AboutCard/AboutCard.jsx
@@ -3,22 +3,24 @@ import portrait from "../../assets/portrait.jpg";
 import styles from "./AboutCard.module.scss";
 
 const AboutCard = () => {
-  const portraitStyle = {
-    backgroundImage: `url(${portrait})`,
-    borderRadius: "10rem",
-    width: "20rem",
-    height: "20rem",
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    boxShadow: "2px 2px 10px #f8f9fa",
-  };
+  // const portraitStyle = {
+  //   backgroundImage: `url(${portrait})`,
+  //   borderRadius: "10rem",
+  //   width: "20rem",
+  //   height: "20rem",
+  //   backgroundPosition: "center",
+  //   backgroundSize: "cover",
+  //   backgroundRepeat: "no-repeat",
+  //   boxShadow: "2px 2px 10px #f8f9fa",
+  // };
 
   return (
     <div className={styles.AboutCard}>
       <h1>About me</h1>
       <div className={styles.AboutCard_Info}>
-        <div style={portraitStyle}></div>
+        <div className={styles.AboutCard_Image}>
+          <img src={portrait} />
+        </div>
         <div className={styles.AboutCard_Info_Para}>
           <p>
             I'm Paul, a full-stack developer based in Sydney, Australia. I love
